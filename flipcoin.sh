b3e657acc309f188c39d1d981c2dfcdedd40569e
geeth22 <geethchandra22@gmail.com>
2020-07-08 18:25:24 +0530

    update commit

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -x
 
+#usecase 1
+
 r=$((RANDOM % 2))
 echo "enter 1 for head and 0 for tails"
 read choice
@@ -16,4 +18,25 @@ then
 	echo "user won"
 else
 	echo "user lost"
+fi
+
+#usecase2
+
+read times
+for((i=0;i<$times;i++))
+do
+	r=$((RANDOM % 2)) 
+	if [ $r -eq 1 ]
+	then
+		heads=$(($heads+1))
+	else 
+		tails=$(($tails+1))
+	fi
+done
+ 
+if [ $heads -gt $tails ]
+then
+	echo "heads won $heads"
+else
+	echo "tails wins $tails"
 fi
