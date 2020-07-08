ddfa4abe6ca7c1b208621c46da2623e24419b1e7
geeth22 <geethchandra22@gmail.com>
2020-07-08 18:26:40 +0530

    update commit

--- flipcoin.sh
+++ flipcoin.sh
@@ -40,3 +40,32 @@ then
 else
 	echo "tails wins $tails"
 fi
+
+#usecase3
+
+heads=0
+tails=0
+
+while [ $heads -le 21 ] && [ $tails -le 21 ]
+do
+	i=0
+	$i=$((i+1))
+	for((i=0;i<100;i++))
+	do
+		r=$((RANDOM % 2)) 
+		if [ $r -eq 1 ]
+		then
+			heads=$(($heads+1))
+		else 
+			tails=$(($tails+1))
+		fi
+	done
+ 
+if [ $heads -gt $tails ]
+then
+	echo "heads won $heads"
+else
+	echo "tails wins $tails"
+fi
+done
+echo "number of rounds $i"
